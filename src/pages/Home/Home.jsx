5866a7cdaf22e4924a95d3daff0df2667ab3004e
Kaiser <kaisarmunna.km15@gmail.com>
2026-02-25 03:39:18 +0600

    banner/hero component (missing custom font)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../../components/header/Header";
+import { Hero } from "./Hero";
 
 export const Home = () => {
   return (
     <div>
       <Header />
+      <Hero />
     </div>
   );
 };
